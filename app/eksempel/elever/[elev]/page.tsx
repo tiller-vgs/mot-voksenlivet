c7e22f9f4ce5d2b540b9d4d9f62bbdd47c7caa30
Jon <128969752+jonp-h@users.noreply.github.com>
2026-09-01 10:05:44 +0200

    feat: add shadcn

--- app/eksempel/elever/[elev]/page.tsx
+++ app/eksempel/elever/[elev]/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 async function Elev({ params }: { params: Promise<{ elev: string }> }) {
@@ -5,7 +7,13 @@ async function Elev({ params }: { params: Promise<{ elev: string }> }) {
   const data = await params;
 
   // TSX / JSX (html med javascript når vi skriver {})
-  return <div>{data.elev}</div>;
+  return (
+    <div>
+      <Link href={`/eksempel/elever/${data.elev}`}>
+        <Button>{data.elev}</Button>
+      </Link>
+    </div>
+  );
 }
 
 export default Elev;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <p className="text-9xl">Hallo Eivind</p>
       <p className="text-white font-bold ">Har du vondt i hodet?</p>
+      <Link href="/eksempel/sofiia">
+        <Button>Klikk her</Button>
+      </Link>
+      <Textarea placeholder="Skriv noe her" />
     </div>
   );
 }
